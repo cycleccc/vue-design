34a8de7388e5a931f546f63ba24994db881bdcac
yaolongfei <2991205548@qq.com>
2024-01-18 15:24:36 +0800

    ✨feat: 添加4.7_调度执行

---
+++ 第4章响应系统的作用与实现/4.7_调度执行/4.7_调度执行.js
@@ -0,0 +1,110 @@
+"use strict";
+// 用一个全局变量存储当前激活的 effect 函数
+let activeEffect;
+// effect 栈
+const effectStack = [];
+function effect(fn, options = {}) {
+    const effectFn = () => {
+        cleanup(effectFn);
+        // 当调用 effect 注册副作用函数时，将副作用函数赋值给 activeEffect
+        activeEffect = effectFn;
+        // 在调用副作用函数之前将当前副作用函数压栈
+        effectStack.push(effectFn);
+        fn();
+        // 在当前副作用函数执行完毕后，将当前副作用函数弹出栈，并把 activeEffect 还原为之前的值
+        effectStack.pop();
+        activeEffect = effectStack[effectStack.length - 1];
+    };
+    // 将 options 挂载到 effectFn 上
+    effectFn.options = options; // 新增
+    // activeEffect.deps 用来存储所有与该副作用函数相关的依赖集合
+    effectFn.deps = [];
+    // 执行副作用函数
+    effectFn();
+}
+function cleanup(effectFn) {
+    // 遍历 effectFn.deps 数组
+    for (let i = 0; i < effectFn.deps.length; i++) {
+        // deps 是依赖集合
+        const deps = effectFn.deps[i];
+        // 将 effectFn 从依赖集合中移除
+        deps.delete(effectFn);
+    }
+    // 最后需要重置 effectFn.deps 数组
+    effectFn.deps.length = 0;
+}
+const bucket = new WeakMap();
+function track(target, key) {
+    // 没有 activeEffect，直接 return
+    if (!activeEffect)
+        return;
+    let depsMap = bucket.get(target);
+    if (!depsMap) {
+        bucket.set(target, (depsMap = new Map()));
+    }
+    let deps = depsMap.get(key);
+    if (!deps) {
+        depsMap.set(key, (deps = new Set()));
+    }
+    // 把当前激活的副作用函数添加到依赖集合 deps 中
+    deps.add(activeEffect);
+    // deps 就是一个与当前副作用函数存在联系的依赖集合
+    // 将其添加到 activeEffect.deps 数组中
+    activeEffect.deps.push(deps); // 新增
+}
+// 在 set 拦截函数内调用 trigger 函数触发变化
+function trigger(target, key) {
+    const depsMap = bucket.get(target);
+    if (!depsMap)
+        return;
+    const effects = depsMap.get(key);
+    const effectsToRun = new Set();
+    effects && effects.forEach((effectFn) => {
+        if (effectFn !== activeEffect) {
+            effectsToRun.add(effectFn);
+        }
+    });
+    effectsToRun.forEach((effectFn) => {
+        var _a;
+        // 如果一个副作用函数存在调度器，则调用该调度器，并将副作用函数作为参数传递
+        if ((_a = effectFn === null || effectFn === void 0 ? void 0 : effectFn.options) === null || _a === void 0 ? void 0 : _a.scheduler) { // 新增
+            effectFn.options.scheduler(effectFn); // 新增
+        }
+        else {
+            // 否则直接执行副作用函数（之前的默认行为）
+            effectFn(); // 新增
+        }
+    });
+}
+const data = { foo: 1 };
+// 代理对象
+const obj = new Proxy(data, {
+    // 拦截读取操作
+    get(target, key) {
+        // 将副作用函数 activeEffect 添加到存储副作用函数的桶中
+        track(target, key);
+        // 返回属性值
+        return target[key];
+    },
+    // 拦截设置操作
+    set(target, key, newVal) {
+        // 设置属性值
+        target[key] = newVal;
+        // 把副作用函数从桶里取出并执行
+        trigger(target, key);
+        return true;
+    }
+});
+effect(() => {
+    console.log(obj.foo);
+}, 
+// options
+{
+    // 调度器 scheduler 是一个函数
+    scheduler(fn) {
+        // 将副作用函数放到宏任务队列中执行
+        setTimeout(fn);
+    }
+});
+obj.foo++;
+console.log('结束了');
