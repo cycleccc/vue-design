d4d9e9d1e18f2dddf53f52a753685eba111138a7
yaolongfei <2991205548@qq.com>
2024-02-19 16:38:43 +0800

    ✨feat: 完善8.3正确地设置元素属性

---
+++ 第8章挂载与更新/8.3_正确地设置元素属性/8.3_正确地设置元素属性.ts
@@ -0,0 +1,131 @@
+interface VNode {
+    type: string;
+    props?: { [key: string]: any }; // 属性对象可以包含任意键值对
+    children: string | VNode[];
+}
+
+interface Options {
+    createElement(tagName: string): HTMLElement;
+    insert(parent: HTMLElement, child: HTMLElement, index?: Node): void;
+    setElementText(element: HTMLElement, text: string): void;
+    patchProps(element: HTMLElement, key: string, prevValue: any, nextValue: any): void;
+}
+function shouldSetAsProps(el: HTMLElement, key: string, value: any) {
+    // 特殊处理
+    if (key === 'form' && el.tagName === 'INPUT') return false
+    // 兜底
+    return key in el
+}
+function createRenderer(options: Options) {
+    // 通过 options 得到操作 DOM 的 API
+    const {
+        createElement,
+        insert,
+        setElementText,
+        patchProps
+    } = options
+    function mountElement(vnode: VNode, container: HTMLElement) {
+        // 调用 createElement 函数创建元素
+        // const el : { [key: string]: any } = createElement(vnode.type)
+        const el = createElement(vnode.type)
+        if (typeof vnode.children === 'string') {
+            // 调用 setElementText  设置元素的文本节点
+            setElementText(el, vnode.children)
+        } else if (Array.isArray(vnode.children)) {
+            // 如果  children 是数组，则遍历每一个子节点，并调用 patch 函数挂载它们
+            vnode.children.forEach(child => {
+                patch(null, child, el)
+            })
+        }
+        if (vnode.props) {
+            // 遍历 vnode.props
+            for (const key in vnode.props) {
+                patchProps(el, key, null, vnode.props[key])
+            }
+        }
+        // 调用 insert 函数将元素插入到容器内
+        insert(el, container)
+    }
+    function patch(n1: any, n2: any, container: any) {
+        if (!n1) {
+            mountElement(n2, container)
+        } else {
+            // n1 存在则是 diff
+        }
+    }
+    function render(vnode: VNode | null, container: HTMLElement | object | any) {
+        if (vnode) {
+            // 新 vnode 存在，将其与旧 vnode 一起传递给 patch 函数，进行打补丁
+            patch(container._vnode, vnode, container);
+        } else {
+            if (container._vnode) {
+                // 旧 vnode 存在，且新 vnode 不存在，说明是卸载（unmount）操作
+                // 只需要将 container 内的 DOM 清空即可
+                container.innerHTML = '';
+            }
+        }
+        // 把 vnode 存储到 container._vnode 下，即后续渲染中的旧 vnode
+        container._vnode = vnode;
+    }
+
+    return {
+        render
+    };
+}
+
+function createAppElement() {
+    // 创建一个新的 div 元素
+    const appDiv = document.createElement('div');
+
+    // 设置 div 的 id 属性为 "app"
+    appDiv.id = 'app';
+
+    // 将创建的 div 元素添加到文档的 body 中
+    document.body.appendChild(appDiv);
+    return appDiv
+}
+
+const vnode: VNode = {
+    type: 'div',
+    props: {
+        id: 'foo'
+    },
+    children: [
+        {
+            type: 'p',
+            children: 'hello'
+        }
+    ]
+}
+
+// 在创建 renderer 时传入配置项
+
+const renderer = createRenderer({
+    createElement(tag) {
+        return document.createElement(tag)
+    },
+    setElementText(el, text) {
+        el.textContent = text
+    },
+    insert(el, parent, anchor: Node) {
+        parent.insertBefore(el, anchor)
+    },
+    // 将属性设置相关操作封装到 patchProps 函数中，并作为渲染器选项传递
+    patchProps(el, key, prevValue, nextValue) {
+        if (shouldSetAsProps(el, key, nextValue)) {
+            const type = typeof (el as any)[key]
+            if (type === 'boolean' && nextValue === '') {
+                (el as any)[key] = true
+            } else {
+                (el as any)[key] = nextValue
+            }
+        } else {
+            el.setAttribute(key, nextValue)
+        }
+    }
+})
+
+
+// 本章节代码ts注解不好搞，又是HTMLElement又是object，后面的章节再规范
+const container = { type: 'root' }
+renderer.render(vnode, document.querySelector('#app') || createAppElement())
