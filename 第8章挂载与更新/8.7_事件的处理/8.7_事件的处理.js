61a44cac14d50673c6c89fb2050fdf0c64d8bf07
yaolongfei <2991205548@qq.com>
2024-02-20 11:10:38 +0800

    ✨feat: 完善8.7事件的处理

--- 第8章挂载与更新/8.7_事件的处理/8.7_事件的处理.js
+++ 第8章挂载与更新/8.7_事件的处理/8.7_事件的处理.js
@@ -82,16 +82,6 @@ function createRenderer(options) {
         render
     };
 }
-const vnode = {
-    type: 'p',
-    props: {
-        // 使用 onXxx 描述事件
-        onClick: () => {
-            alert('clicked');
-        }
-    },
-    children: 'text'
-};
 // 在创建 renderer 时传入配置项
 const renderer = createRenderer({
     createElement(tag) {
@@ -107,16 +97,45 @@ const renderer = createRenderer({
     patchProps(el, key, prevValue, nextValue) {
         // 匹配以 on 开头的属性，视其为事件
         if (/^on/.test(key)) {
-            // 根据属性名称得到对应的事件名称，例如 onClick ---> click
+            // 定义 el._vei 为一个对象，存在事件名称到事件处理函数的映射
+            const invokers = el._vei || (el._vei = {});
+            //根据事件名称获取 invoker
+            let invoker = invokers[key];
             const name = key.slice(2).toLowerCase();
-            // 绑定事件，nextValue 为事件处理函数
-            el.addEventListener(name, nextValue);
-            // 如果key为class直接设置className比用setAtribute更快
+            if (nextValue) {
+                if (!invoker) {
+                    // 如果没有 invoker，则将一个伪造的 invoker 缓存到 el._vei 中
+                    // vei 是 vue event invoker 的首字母缩写
+                    invoker = el._vei[key] = (e) => {
+                        // 如果 invoker.value 是数组，则遍历它并逐个调用事件处理函数
+                        if (Array.isArray(invoker.value)) {
+                            invoker.value.forEach((fn) => fn(e));
+                        }
+                        else {
+                            // 否则直接作为函数调用
+                            // 当伪造的事件处理函数执行时，会执行真正的事件处理函数
+                            invoker.value(e);
+                        }
+                    };
+                    // 将真正的事件处理函数赋值给 invoker.value
+                    invoker.value = nextValue;
+                    // 绑定 invoker 作为事件处理函数
+                    el.addEventListener(name, invoker);
+                }
+                else {
+                    // 如果 invoker 存在，意味着更新，并且只需要更新 invoker.value 的值即可
+                    invoker.value = nextValue;
+                }
+            }
+            else if (invoker) {
+                // 新的事件绑定函数不存在，且之前绑定的 invoker 存在，则移除绑定
+                el.removeEventListener(name, invoker);
+            }
         }
-        if (key === 'class') {
+        else if (key === 'class') {
             el.className = nextValue || '';
         }
-        if (shouldSetAsProps(el, key, nextValue)) {
+        else if (shouldSetAsProps(el, key, nextValue)) {
             const type = typeof el[key];
             if (type === 'boolean' && nextValue === '') {
                 el[key] = true;
@@ -130,9 +149,35 @@ const renderer = createRenderer({
         }
     }
 });
-// 本章节代码ts注解不好搞，又是HTMLElement又是object，后面的章节再规范
-const container = { type: 'root' };
+// 不同事件名称的vnode
+// const vnode = {
+//     type: 'p',
+//     props: {
+//         onClick: () => {
+//             alert('clicked')
+//         },
+//         onContextmenu: () => {
+//             alert('contextmenu')
+//         }
+//     },
+//     children: 'text'
+// }
+// 相同事件名称不同函数的vnode
+const vnode = {
+    type: 'p',
+    props: {
+        onClick: [
+            // 第一个事件处理函数
+            () => {
+                alert('clicked 1');
+            },
+            // 第二个事件处理函数
+            () => {
+                alert('clicked 2');
+            }
+        ]
+    },
+    children: 'text'
+};
 // 初次挂载
 renderer.render(vnode, document.querySelector('#app'));
-// 新 vnode 为 null，意味着卸载之前渲染的内容
-renderer.render(null, document.querySelector('#app'));
--- 第8章挂载与更新/8.7_事件的处理/8.7_事件的处理.ts
+++ 第8章挂载与更新/8.7_事件的处理/8.7_事件的处理.ts
@@ -5,6 +5,10 @@ interface VNode {
     el?: HTMLElement;
 }
 
+interface HTMLElement {
+    _vei: any
+}
+
 interface Options {
     createElement(tagName: string): HTMLElement;
     insert(parent: HTMLElement, child: HTMLElement, index?: Node): void;
@@ -95,19 +99,7 @@ function createRenderer(options: Options) {
     };
 }
 
-const vnode = {
-    type: 'p',
-    props: {
-        // 使用 onXxx 描述事件
-        onClick: () => {
-            alert('clicked')
-        }
-    },
-    children: 'text'
-}
-
 // 在创建 renderer 时传入配置项
-
 const renderer = createRenderer({
     createElement(tag) {
         return document.createElement(tag)
@@ -122,15 +114,40 @@ const renderer = createRenderer({
     patchProps(el, key, prevValue, nextValue) {
         // 匹配以 on 开头的属性，视其为事件
         if (/^on/.test(key)) {
-            // 根据属性名称得到对应的事件名称，例如 onClick ---> click
+            // 定义 el._vei 为一个对象，存在事件名称到事件处理函数的映射
+            const invokers = el._vei || (el._vei = {})
+            //根据事件名称获取 invoker
+            let invoker = invokers[key]
             const name = key.slice(2).toLowerCase()
-            // 绑定事件，nextValue 为事件处理函数
-            el.addEventListener(name, nextValue as EventListenerOrEventListenerObject)
-            // 如果key为class直接设置className比用setAtribute更快
-        } if (key === 'class') {
+            if (nextValue) {
+                if (!invoker) {
+                    // 如果没有 invoker，则将一个伪造的 invoker 缓存到 el._vei 中
+                    // vei 是 vue event invoker 的首字母缩写
+                    invoker = el._vei[key] = (e: string | EventListenerOrEventListenerObject | null) => {
+                        // 如果 invoker.value 是数组，则遍历它并逐个调用事件处理函数
+                        if (Array.isArray(invoker.value)) {
+                            invoker.value.forEach((fn: Function) => fn(e))
+                        } else {
+                            // 否则直接作为函数调用
+                            // 当伪造的事件处理函数执行时，会执行真正的事件处理函数
+                            invoker.value(e)
+                        }
+                    }
+                    // 将真正的事件处理函数赋值给 invoker.value
+                    invoker.value = nextValue
+                    // 绑定 invoker 作为事件处理函数
+                    el.addEventListener(name, invoker)
+                } else {
+                    // 如果 invoker 存在，意味着更新，并且只需要更新 invoker.value 的值即可
+                    invoker.value = nextValue
+                }
+            } else if (invoker) {
+                // 新的事件绑定函数不存在，且之前绑定的 invoker 存在，则移除绑定
+                el.removeEventListener(name, invoker)
+            }
+        } else if (key === 'class') {
             el.className = nextValue as string || ''
-        }
-        if (shouldSetAsProps(el, key, nextValue)) {
+        } else if (shouldSetAsProps(el, key, nextValue)) {
             const type = typeof (el as any)[key]
             if (type === 'boolean' && nextValue === '') {
                 (el as any)[key] = true
@@ -143,10 +160,37 @@ const renderer = createRenderer({
     }
 })
 
+// 不同事件名称的vnode
+// const vnode = {
+//     type: 'p',
+//     props: {
+//         onClick: () => {
+//             alert('clicked')
+//         },
+//         onContextmenu: () => {
+//             alert('contextmenu')
+//         }
+//     },
+//     children: 'text'
+// }
 
-// 本章节代码ts注解不好搞，又是HTMLElement又是object，后面的章节再规范
-const container = { type: 'root' }
+// 相同事件名称不同函数的vnode
+const vnode = {
+    type: 'p',
+    props: {
+        onClick: [
+            // 第一个事件处理函数
+            () => {
+                alert('clicked 1')
+            },
+            // 第二个事件处理函数
+            () => {
+                alert('clicked 2')
+            }
+        ]
+    },
+    children: 'text'
+}
+
 // 初次挂载
 renderer.render(vnode, document.querySelector('#app'))
-// 新 vnode 为 null，意味着卸载之前渲染的内容
-renderer.render(null, document.querySelector('#app'))
